94c50174cdfb0271c7d587d1039e0b8d9597f311
Ian Castillo <ian.abraham.castillo@gmail.com>
2023-08-26 19:58:08 -0600

    Adds Logic to the View Model
    Creates WeatherViewModel as an Observable Object that contains fetchWeatherData that we are gonna use later on our SwiftUI View.

--- Best Weather Forecast App/ViewModel/WeatherViewModel.swift
+++ Best Weather Forecast App/ViewModel/WeatherViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+class WeatherViewModel: ObservableObject {
+    @Published var realtimeWeather: RealtimeWeather?
+    @Published var forecast: [ForecastDay] = []
+    @Published var isLoading = false
+    @Published var error: NetworkError?
+
+    private var weatherService = WeatherService()
+
+    func fetchWeatherData(forCity city: String) {
+        isLoading = true
+        weatherService.fetchRealtimeWeather(forCity: city) { [weak self] result in
+            switch result {
+            case .success(let weather):
+                DispatchQueue.main.async {
+                    self?.realtimeWeather = weather
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.error = error
+                }
+            }
+
+            self?.weatherService.fetchForecast(forCity: city) { [weak self] result in
+                switch result {
+                case .success(let forecastResponse):
+                    DispatchQueue.main.async {
+                        self?.forecast = forecastResponse.forecastday
+                        self?.isLoading = false
+                    }
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.error = error
+                        self?.isLoading = false
+                    }
+                }
+            }
+        }
+    }
+}
